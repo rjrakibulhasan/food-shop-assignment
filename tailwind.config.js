697382c5f91f363f7a90a4c399bec9674908a212
rjrakibulhasan <121838257+rjrakibulhasan@users.noreply.github.com>
2023-07-25 22:16:48 +0600

    navbar done

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      color: {
+        'bgColor-header': ': #F85559;'
+      }
+    },
+  },
+  plugins: [],
+}
+
